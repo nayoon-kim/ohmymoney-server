7680e2814109bccd2fdd983ce22d0fb7b8d288ea
김나윤 <belloyv@gmail.com>
2020-12-24 00:32:09 +0900

    test

--- src/main/java/com/najinji/ohmymoneyserver/web/WebRestController.java
+++ src/main/java/com/najinji/ohmymoneyserver/web/WebRestController.java
@@ -2,8 +2,9 @@ package com.najinji.ohmymoneyserver.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class WebRestController {
     @GetMapping("/hello")
     public String hello() {
